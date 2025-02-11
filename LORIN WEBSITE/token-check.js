05dbadbfea3cb3b946d94f6c6da5171d8e9ab5c9
Erik Spinnler <erik.spinnler@feusibildungszentrum.ch>
2025-02-11 14:58:51 +0100

    Delete LORIN WEBSITE directory

--- LORIN WEBSITE/token-check.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const validTokens = ["geheimerToken123", "zweiterToken456"]; // Hier gültige Tokens eintragen
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("token");
-
-    if (!token || !validTokens.includes(token)) {
-        document.body.innerHTML = '<div class="error">Zugriff verweigert! Ungültiger oder fehlender Token.</div>';
-    }
-});
